095375fc024b28c432500ae37def68bf8f2895b7
tjmuscles <tjmuscles@yahoo.com>
2022-07-15 14:45:43 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
+import { EMPTY, Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 
+const failingHttpRequest$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.error( new Error('Timeout'));
+  }, 3000);
+
+});
+
+console.log('App started');
+
+failingHttpRequest$
+.pipe(
+  catchError(() => EMPTY)
+)
+.subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('COMPLETED')
+});
